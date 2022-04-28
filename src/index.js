baf24b4bddc188e46553162bf74b15fc1f8947c3
Joe Eames <joeeames@gmail.com>
2022-04-28 13:39:42 -0600

    first checkin - module 1 start

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const cart = [];
+
+function log(message) {
+  console.log(message);
+}
+
+function addToCart(item) {
+  cart.push(item);
+  log("added: " + item);
+}
+
+function removeFromCart(idx) {
+  cart.splice(idx, 1);
+  log("removed: ", idx);
+}
+
+addToCart("Waterproof Boots");
