b2587c0463c082466ed0a53385c8bdc47a9e3aa7
Joe Eames <joeeames@gmail.com>
2022-04-28 18:06:39 -0600

    added dev server config

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,9 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  devtool: "inline-source-map",
+  mode: "development",
+  devServer: {
+    static: "./dist",
+  },
 };
