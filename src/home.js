54d926a0941accc1e749a538afae0f0c88a70dd8
Joe Eames <joeeames@gmail.com>
2022-04-29 14:19:24 -0600

    added config for multiple entry points

---
+++ src/home.js
@@ -0,0 +1,8 @@
+const util = require("./util");
+
+function notificationRegister(email) {
+  util.log("registering " + email);
+  // send registration HTTP request
+}
+
+notificationRegister("joe@joe.com");
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: ["./src/index.js", "./src/home.js"],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   devtool: "inline-source-map",
   mode: "development",
   devServer: {
     static: "./dist",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
 };
